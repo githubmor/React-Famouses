d42f37cb6ea69e205a2f566c4d2ff15ff201f3bf
githubmor <m.darzi@pooyasamaneh.net>
2022-08-29 10:47:47 +0430

    create IranMap.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React, { useState} from 'react';
-import { MorphReplace } from 'react-svg-morph';
-const Markdown:Element[] = []
-for (let i = 1; i <= 21; i++) {
-    Markdown[i] = require(`./components/maps/iran${i}`).default
-}
+import IranMap from "./components/IranMap";
+import {useState} from "react";
+
 function App() {
-    const [check, setCheck] = useState<number>(1);
-    const Iran = require(`./components/maps/iran${check}`).default
+    const [number, setNumber] = useState<number>(1);
+
     return (
         <div className="App">
-            {/*<MorphReplace duration={3000} rotation={"none"} width={300} height={300}>*/}
-                <Iran  key={check}/>
-            {/*</MorphReplace>*/}
-            <p>{`check is ${check}`}</p>
-            <input type={"range"} min={1} max={21} onChange={(e)=>setCheck(+e.target.value)} />
+            <IranMap num={number} fill={"#1571a2"}/>
+            <input type={"range"} min={1} max={21} onChange={(e) => setNumber(+e.target.value)}/>
         </div>
     );
 }
