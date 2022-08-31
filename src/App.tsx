5ad60f4220c069392cd7c9f69cd87669578cde6b
githubmor <m.darzi@pooyasamaneh.net>
2022-08-31 09:51:31 +0430

    add map of world

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import IranMap from "./components/IranMap";
 import {useState} from "react";
-
+import map from "./components/map-bg.png"
 function App() {
     const [number, setNumber] = useState<number>(1);
 
     return (
         <div className="App">
-            <IranMap num={number} fill={"#1571a2"}/>
-            <input type={"range"} min={1} max={21} onChange={(e) => setNumber(+e.target.value)}/>
+            <img style={{position:"fixed"}} src={map} alt={"sd"} />
+            <IranMap  num={number} fill={"#1571a2"}/>
+            <input style={{position:"absolute",right:"0"}} type={"range"} min={1} max={21} onChange={(e) => setNumber(+e.target.value)}/>
         </div>
     );
 }
--- src/components/IranMap.tsx
+++ src/components/IranMap.tsx
@@ -36,12 +36,12 @@ const paths: Record<number, string[]> = {
 function IranMap({num = 1, fill = "green", stroke = "black"}: { num: number, fill?: string, stroke?: string }) {
 
     return (
-        <svg width="726" height="435" viewBox="0 0 726 435" xmlns="http://www.w3.org/2000/svg">
+        <svg  style={{position:"fixed",top: "151px",left: "162px"}} width="726" height="435" viewBox="0 0 726 435" xmlns="http://www.w3.org/2000/svg">
             <g id={`iran${num}`}>
                 {
                     paths[num].map(
                         (data, index) =>
-                            <path id={`path${num}-${index}`} d={data} fill={fill} stroke={stroke}/>
+                            <path id={`path${num}-${index}`} d={data} fill={fill} opacity={0.6} stroke={stroke}/>
                     )
                 }
             </g>
