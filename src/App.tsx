8e0029062bdf1c9f728ad210aa43db151c2c24a4
githubmor <pc2man@gmail.com>
2022-09-03 22:35:46 +0430

    add slide vertically

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import IranMap from "./components/IranMap";
-import {useState} from "react";
-import map from "./components/map-bg.png"
+import { useState } from "react";
+import map from "./components/map-bg.png";
 function App() {
-    const [number, setNumber] = useState<number>(1);
+  const [number, setNumber] = useState<number>(1);
 
-    return (
-        <div className="App">
-            <img style={{position:"fixed"}} src={map} alt={"sd"} />
-            <IranMap  num={number} fill={"#1571a2"}/>
-            <input style={{position:"absolute",right:"0"}} type={"range"} min={1} max={21} onChange={(e) => setNumber(+e.target.value)}/>
-        </div>
-    );
+  // useEffect(() => {
+  //   const onScroll = () => setNumber(window.pageYOffset);
+  //   // clean up code
+  //   window.removeEventListener("scroll", onScroll);
+  //   window.addEventListener("scroll", onScroll, { passive: true });
+  //   return () => window.removeEventListener("scroll", onScroll);
+  // }, []);
+
+  // console.log(number);
+
+  return (
+    <div className="App">
+      <img style={{ position: "fixed" }} src={map} alt={"sd"} />
+      <IranMap num={number} fill={"#1571a2"} />
+      <input
+        style={{
+          WebkitAppearance: "slider-vertical",
+          width: "20px",
+          height: "95%",
+          position: "absolute",
+          right: "0",
+        }}
+        value={number}
+        type={"range"}
+        min={1}
+        max={21}
+        onChange={(e) => setNumber(+e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
